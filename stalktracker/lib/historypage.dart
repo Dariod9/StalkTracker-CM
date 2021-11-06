8ccac2583b2c5b0c79e71e398e44f8120b174620
Pedro <pedralmeida.22.99@gmail.com>
2021-11-06 20:07:30 +0000

    choose date in history page

--- stalktracker/lib/historypage.dart
+++ stalktracker/lib/historypage.dart
@@ -1,7 +1,30 @@
 import 'package:url_launcher/url_launcher.dart';
 import 'package:flutter/material.dart';
 
-class historypage extends StatelessWidget {
+
+class fromDateWidget extends StatelessWidget {
+  final String text;
+
+  fromDateWidget(this.text);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text("Stalkers from: " + text,
+      style: TextStyle(color: Colors.white, fontSize: 22),);
+  }
+}
+
+class historypage extends StatefulWidget {
+
+  @override
+  State<historypage> createState() => _historypageState();
+}
+
+class _historypageState extends State<historypage> {
+
+  DateTime _selectedDate = DateTime.now();
+  final _connectionsHistory = ["a", "b", "c", "d", "a", "b", "c", "d", "b", "c", "d", "a", "b", "c", "d"];
+
 
   void mailLaunch(command) async {
     if (await canLaunch(command)) {
@@ -9,6 +32,38 @@ class historypage extends StatelessWidget {
     } else {
       print('Could not launch $command');
     }
+  }
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+        context: context,
+        initialDate: _selectedDate,
+        firstDate: DateTime(2020, 1),
+        lastDate: DateTime.now());
+
+    if (picked != null && picked != _selectedDate){
+      setState(() {
+        _selectedDate = picked;
+      });
+    }
+  }
+
+  Widget _buildConnectionsHistory(){
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        shrinkWrap: true,
+        physics: ScrollPhysics(),
+        itemCount: _connectionsHistory.length,
+        itemBuilder: (context, index) {
+          return _buildRow(_connectionsHistory[index]);
+        }
+    );
+  }
+
+  Widget _buildRow(String s){
+    return ListTile(
+      title: new Text(s, style: TextStyle(color: Colors.white),),
+    );
   }
 
   @override
@@ -32,41 +87,16 @@ class historypage extends StatelessWidget {
             padding: EdgeInsets.all(3),
             child: ListView(
               children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    'Choose date:',
-                    style: TextStyle(color: Colors.white, fontSize: 25),
-                  ),
+                ListTile(
+                  title: fromDateWidget("${_selectedDate.toString()}".split(" ")[0]),
+                  trailing: IconButton(
+                      icon: Icon(Icons.date_range, color: Colors.white,),
+                      onPressed: () => _selectDate(context)),
                 ),
+                // Expanded(child: ListView(children: [_buildConnectionsHistory()],)),
                 Row(
                   children: [
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Icon(
-                        Icons.today,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ],
-                ),
-                Row(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Icon(
-                        Icons.phone,
-                        color: Colors.white,
-                      ),
-                    ),
-                    TextButton(
-                        onPressed: () {
-                          mailLaunch('tel:+917989772884');
-                        },
-                        child: Text(
-                          '+91 79897 72884',
-                          style: TextStyle(fontSize: 20),
-                        )),
+                    new Expanded(child: _buildConnectionsHistory()),
                   ],
                 ),
               ],
